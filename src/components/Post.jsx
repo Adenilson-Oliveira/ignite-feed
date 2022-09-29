46f795b916fc05013e5101b015aaff7d9f4acf09
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2022-09-29 06:08:39 -0300

    header e conteudo do componente Post concluido

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@ import styles from './Post.module.css'
 
 export const Post = () => {
   return (
-    <article>
+    <article className={styles.post}>
       <header>
         <div className={styles.author}>
           <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/81540069?v=4" />
@@ -22,9 +22,13 @@ export const Post = () => {
           Acabei de subir mais um projeto no meu portifa. É um projeto que fiz  no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
 
-        <p>👉 <a href="">jane.design/doctorcare</a></p>
+        <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
 
-        <p> <a href="">#novoprojeto #nlw #rocketseat</a> </p>
+        <p> 
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
       </div>
     </article>
   )
