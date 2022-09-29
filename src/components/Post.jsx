5717718555606a7a88d5c6369be11d1f3d2140f2
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2022-09-29 07:15:35 -0300

    formulario de comentários concluido parte estrutura e visual

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -17,19 +17,28 @@ export const Post = () => {
 
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
-
         <p>
           Acabei de subir mais um projeto no meu portifa. É um projeto que fiz  no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
-
         <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-
         <p> 
           <a href="">#novoprojeto</a>{' '}
           <a href="">#nlw</a>{' '}
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea
+          placeholder='Deixe um comentário'
+        />
+        
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+        
+      </form>
     </article>
   )
 }
